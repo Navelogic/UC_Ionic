cd62689d35ab5852d17a407d3a4101280f1b178c
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-08 11:05:49 -0300

    Finalização

--- Tutorial2/src/app/app.component.ts
+++ Tutorial2/src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-    {title: 'Perfil', url: '/folder/Perfil', icon: 'person'},
+    { title: 'Perfil', url: '/folder/Perfil', icon: 'person'},
+    { title: 'Navegação', url: 'navegacao'}
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
